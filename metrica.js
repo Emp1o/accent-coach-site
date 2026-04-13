c826e86f2c30f932be9988064921cd80380d2a13
Emp1o <salitovmaksim@gmail.com>
2026-04-13 12:51:59 +0300

    Add files via upload

--- metrica.js
+++ metrica.js
@@ -1,40 +1,30 @@
 /*
-Что нужно сделать:
 1. Создай счётчик в Яндекс Метрике.
-2. Вставь код счётчика в index.html перед этим скриптом.
-3. Замени METRICA_COUNTER_ID на свой ID.
-4. Создай цели:
+2. Вставь код счётчика в index.html перед подключением этого файла.
+3. Замени METRICA_COUNTER_ID на свой реальный ID.
+4. Создай в Метрике цели:
    - click_install
-   - click_github
    - click_install_bottom
+   - click_github
 */
-
 (function () {
-  var counterId = '108492491';
+  var counterId = 'METRICA_COUNTER_ID';
 
   function sendGoal(name) {
     if (window.ym) ym(counterId, 'reachGoal', name);
   }
 
   var installBtn = document.getElementById('install-btn');
-  var githubBtn = document.getElementById('github-btn');
   var installBottomBtn = document.getElementById('install-btn-bottom');
+  var githubBtn = document.getElementById('github-btn');
 
   if (installBtn) {
-    installBtn.addEventListener('click', function () {
-      sendGoal('click_install');
-    });
+    installBtn.addEventListener('click', function () { sendGoal('click_install'); });
   }
-
-  if (githubBtn) {
-    githubBtn.addEventListener('click', function () {
-      sendGoal('click_github');
-    });
-  }
-
   if (installBottomBtn) {
-    installBottomBtn.addEventListener('click', function () {
-      sendGoal('click_install_bottom');
-    });
+    installBottomBtn.addEventListener('click', function () { sendGoal('click_install_bottom'); });
+  }
+  if (githubBtn) {
+    githubBtn.addEventListener('click', function () { sendGoal('click_github'); });
   }
 })();
