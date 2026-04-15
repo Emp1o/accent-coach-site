33f813630e083632639801c06f07d8e9c2d1c44d
Emp1o <salitovmaksim@gmail.com>
2026-04-15 15:10:47 -0700

    Update metrica.js

--- metrica.js
+++ metrica.js
@@ -9,6 +9,7 @@
   var installBottomBtn = document.getElementById('install-btn-bottom');
   var githubBtn = document.getElementById('github-btn');
   var feedbackBtn = document.getElementById('feedback-btn');
+  var feedbackBtnTop = document.getElementById('feedback-btn-top');
 
   if (installBtn) {
     installBtn.addEventListener('click', function () { sendGoal('click_install'); });
@@ -19,9 +20,10 @@
   if (githubBtn) {
     githubBtn.addEventListener('click', function () { sendGoal('click_github'); });
   }
+  if (feedbackBtnTop) {
+    feedbackBtnTop.addEventListener('click', function () { sendGoal('click_feedback_top'); });
+  }
   if (feedbackBtn) {
-  feedbackBtn.addEventListener('click', function () {
-    sendGoal('click_feedback');
-  });
+  feedbackBtn.addEventListener('click', function () { sendGoal('click_feedback'); });
   }
 })();
